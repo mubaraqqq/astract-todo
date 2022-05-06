f2a6668413a8a9ab9cbbccf9a0c08d253e60b401
Mubaraqqq <ayomidemusty@gmail.com>
2022-05-06 22:27:23 +0100

    implemented sign up with firebase authentication

---
+++ src/auth/signup.js
@@ -0,0 +1,17 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+export const firebaseSignup = (email, password) => {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
+}
--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,15 +1,12 @@
 import React, { useState } from 'react';
 import { Paper, Typography, TextField, Box , Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import analytics from '../config/firebase-config';
+import { firebaseSignup } from '../auth/signup';
 
 const Signup = () => {
-    const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-    };
 
     const handleEmailChange = (e) => {
         setEmail(e.target.value);
@@ -17,19 +14,23 @@ const Signup = () => {
 
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
-    }
+    };
+
+    const signUpUser = () => {
+      console.log(email, password);
+      firebaseSignup(email, password);
+    };
 
   return (
     <div className="App">
         <Box>
           <Paper elevation={3} className='sign-in-container' sx={{width: '500px', height: '300px', padding: '1em'}}>
             <Typography variant='h4'>Create an Account</Typography>
-            <form action="">
+            <form action="" onSubmit={signUpUser}>
               <Box className='form'>
-              <Box className='form-input'><TextField label='Full Name' variant='standard' value={name} onChange={handleNameChange} /></Box>
-                <Box className='form-input'><TextField label='Email' variant='standard' value={email} onChange={handleEmailChange} /></Box>
-                <Box className='form-input'><TextField label='Password' type='password' variant='standard' value={password} onChange={handlePasswordChange} /></Box>
-                <Button color='primary' variant='contained'>Submit</Button>
+                <Box className='form-input'><TextField required label='Email' variant='standard' value={email} onChange={handleEmailChange} /></Box>
+                <Box className='form-input'><TextField required label='Password' type='password' variant='standard' value={password} onChange={handlePasswordChange} /></Box>
+                <Button color='primary' variant='contained' onClick={signUpUser}>Sign Up</Button>
               </Box>
             </form>
             <Box><Link to='/'><Typography variant='caption'>Already have an Account?</Typography></Link></Box>
